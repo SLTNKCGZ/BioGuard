a6fdcc889dc0a690253ad13822f0097110fae058
Hatice-Bekkaya <bekkayahatice@gmail.com>
2025-08-02 14:47:26 +0300

    main.dart

--- Frontend/bio_guard/lib/main.dart
+++ Frontend/bio_guard/lib/main.dart
@@ -5,7 +5,8 @@ import 'screens/login_page.dart';
 import 'screens/register_page.dart';
 import 'screens/symptom_entry_page.dart';
 import 'screens/health_datas_page.dart';
-import 'screens/lab_results_page.dart;
+import 'screens/lab_results_page.dart';
+import 'screens/past_lab_results_page.dart';
 
 
 void main() {
@@ -26,6 +27,7 @@ class SaglikUygulamasi extends StatelessWidget {
     );
   }
 }
+
 
 
 
